c12c9c073d5e1a50d156b08c10af112dc4807abf
Moritz Biering <moritzbiering.mb@gmail.com>
2024-12-23 20:24:48 +0100

    2024 - day21

---
+++ 2024/day21/main.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/zMoooooritz/advent-of-code/cast"
+	"github.com/zMoooooritz/advent-of-code/util"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+type Action struct {
+	src  byte
+	trgt byte
+}
+
+// LUT idea / adaption from https://gist.github.com/expectancyViolation/f44afc498868e18fce3838764d927ee7
+var LUT = map[Action]string{
+	{'A', '0'}: "<A",
+	{'0', 'A'}: ">A",
+	{'A', '1'}: "^<<A",
+	{'1', 'A'}: ">>vA",
+	{'A', '2'}: "<^A",
+	{'2', 'A'}: "v>A",
+	{'A', '3'}: "^A",
+	{'3', 'A'}: "vA",
+	{'A', '4'}: "^^<<A",
+	{'4', 'A'}: ">>vvA",
+	{'A', '5'}: "<^^A",
+	{'5', 'A'}: "vv>A",
+	{'A', '6'}: "^^A",
+	{'6', 'A'}: "vvA",
+	{'A', '7'}: "^^^<<A",
+	{'7', 'A'}: ">>vvvA",
+	{'A', '8'}: "<^^^A",
+	{'8', 'A'}: "vvv>A",
+	{'A', '9'}: "^^^A",
+	{'9', 'A'}: "vvvA",
+	{'0', '1'}: "^<A",
+	{'1', '0'}: ">vA",
+	{'0', '2'}: "^A",
+	{'2', '0'}: "vA",
+	{'0', '3'}: "^>A",
+	{'3', '0'}: "<vA",
+	{'0', '4'}: "^<^A",
+	{'4', '0'}: ">vvA",
+	{'0', '5'}: "^^A",
+	{'5', '0'}: "vvA",
+	{'0', '6'}: "^^>A",
+	{'6', '0'}: "<vvA",
+	{'0', '7'}: "^^^<A",
+	{'7', '0'}: ">vvvA",
+	{'0', '8'}: "^^^A",
+	{'8', '0'}: "vvvA",
+	{'0', '9'}: "^^^>A",
+	{'9', '0'}: "<vvvA",
+	{'1', '2'}: ">A",
+	{'2', '1'}: "<A",
+	{'1', '3'}: ">>A",
+	{'3', '1'}: "<<A",
+	{'1', '4'}: "^A",
+	{'4', '1'}: "vA",
+	{'1', '5'}: "^>A",
+	{'5', '1'}: "<vA",
+	{'1', '6'}: "^>>A",
+	{'6', '1'}: "<<vA",
+	{'1', '7'}: "^^A",
+	{'7', '1'}: "vvA",
+	{'1', '8'}: "^^>A",
+	{'8', '1'}: "<vvA",
+	{'1', '9'}: "^^>>A",
+	{'9', '1'}: "<<vvA",
+	{'2', '3'}: ">A",
+	{'3', '2'}: "<A",
+	{'2', '4'}: "<^A",
+	{'4', '2'}: "v>A",
+	{'2', '5'}: "^A",
+	{'5', '2'}: "vA",
+	{'2', '6'}: "^>A",
+	{'6', '2'}: "<vA",
+	{'2', '7'}: "<^^A",
+	{'7', '2'}: "vv>A",
+	{'2', '8'}: "^^A",
+	{'8', '2'}: "vvA",
+	{'2', '9'}: "^^>A",
+	{'9', '2'}: "<vvA",
+	{'3', '4'}: "<<^A",
+	{'4', '3'}: "v>>A",
+	{'3', '5'}: "<^A",
+	{'5', '3'}: "v>A",
+	{'3', '6'}: "^A",
+	{'6', '3'}: "vA",
+	{'3', '7'}: "<<^^A",
+	{'7', '3'}: "vv>>A",
+	{'3', '8'}: "<^^A",
+	{'8', '3'}: "vv>A",
+	{'3', '9'}: "^^A",
+	{'9', '3'}: "vvA",
+	{'4', '5'}: ">A",
+	{'5', '4'}: "<A",
+	{'4', '6'}: ">>A",
+	{'6', '4'}: "<<A",
+	{'4', '7'}: "^A",
+	{'7', '4'}: "vA",
+	{'4', '8'}: "^>A",
+	{'8', '4'}: "<vA",
+	{'4', '9'}: "^>>A",
+	{'9', '4'}: "<<vA",
+	{'5', '6'}: ">A",
+	{'6', '5'}: "<A",
+	{'5', '7'}: "<^A",
+	{'7', '5'}: "v>A",
+	{'5', '8'}: "^A",
+	{'8', '5'}: "vA",
+	{'5', '9'}: "^>A",
+	{'9', '5'}: "<vA",
+	{'6', '7'}: "<<^A",
+	{'7', '6'}: "v>>A",
+	{'6', '8'}: "<^A",
+	{'8', '6'}: "v>A",
+	{'6', '9'}: "^A",
+	{'9', '6'}: "vA",
+	{'7', '8'}: ">A",
+	{'8', '7'}: "<A",
+	{'7', '9'}: ">>A",
+	{'9', '7'}: "<<A",
+	{'8', '9'}: ">A",
+	{'9', '8'}: "<A",
+	{'<', '^'}: ">^A",
+	{'^', '<'}: "v<A",
+	{'<', 'v'}: ">A",
+	{'v', '<'}: "<A",
+	{'<', '>'}: ">>A",
+	{'>', '<'}: "<<A",
+	{'<', 'A'}: ">>^A",
+	{'A', '<'}: "v<<A",
+	{'^', 'v'}: "vA",
+	{'v', '^'}: "^A",
+	{'^', '>'}: "v>A",
+	{'>', '^'}: "<^A",
+	{'^', 'A'}: ">A",
+	{'A', '^'}: "<A",
+	{'v', '>'}: ">A",
+	{'>', 'v'}: "<A",
+	{'v', 'A'}: "^>A",
+	{'A', 'v'}: "<vA",
+	{'>', 'A'}: "^A",
+	{'A', '>'}: "vA",
+}
+
+type Sequence struct {
+	code  string
+	depth int
+}
+
+var cache = map[Sequence]int{}
+
+func calcSequenceLength(sequence Sequence) int {
+	if v, ok := cache[sequence]; ok {
+		return v
+	}
+
+	length := 0
+	if sequence.depth == 0 {
+		length = len(sequence.code)
+	} else {
+		current := 'A'
+		for _, next := range sequence.code {
+			moveCount := calcMoveCount(byte(current), byte(next), sequence.depth)
+			current = next
+			length += moveCount
+		}
+	}
+
+	cache[sequence] = length
+	return length
+}
+
+func calcMoveCount(current, next byte, depth int) int {
+	if current == next {
+		return 1
+	}
+	newCode := LUT[Action{current, next}]
+	return calcSequenceLength(Sequence{newCode, depth - 1})
+}
+
+func part1(input string) int {
+	codes := parseInput(input)
+
+	result := 0
+	for _, code := range codes {
+		numeric := cast.ToInt(strings.TrimSuffix(code, "A"))
+		length := calcSequenceLength(Sequence{code, 3})
+		result += numeric * length
+	}
+	return result
+}
+
+func part2(input string) int {
+	codes := parseInput(input)
+
+	result := 0
+	for _, code := range codes {
+		numeric := cast.ToInt(strings.TrimSuffix(code, "A"))
+		length := calcSequenceLength(Sequence{code, 26})
+		result += numeric * length
+	}
+	return result
+}
+
+func parseInput(input string) (ans []string) {
+	for _, line := range strings.Split(input, "\n") {
+		ans = append(ans, line)
+	}
+	return ans
+}
